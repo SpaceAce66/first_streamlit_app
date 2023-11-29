945e71afb113270a2e1e7057d47989c586871ebf
SpaceAce66 <152376804+SpaceAce66@users.noreply.github.com>
2023-11-29 08:57:13 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ streamlit.title('Healthy Food Ideas')
 streamlit.title('...or, simply, foods my kids won\'t eat')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐓 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacodo Toast')
 
